1d28dad6ab457bbda75cdd3518517f6f88d359be
Sami_aZeus <sami.ezzerouali@hotmail.com>
2019-05-11 11:05:33 +0200

    obd async

--- backEnd/tests/test_odb.py
+++ backEnd/tests/test_odb.py
@@ -1,8 +1,16 @@
-import odb
+import obd
+import time
 
-connection = obd.OBD() 
-cmd = obd.commends.RPM
+connection = obd.Async()
 
-response = connection.query(cmd)
+# a callback that prints every new value to the console
+def new_rpm(r):
+    print r.value
 
-print(response.value)
+connection.watch(obd.commands.RPM, callback=new_rpm)
+connection.start()
+
+# the callback will now be fired upon receipt of new values
+
+time.sleep(60)
+connection.stop()
